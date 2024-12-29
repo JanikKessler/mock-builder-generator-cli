b6363ca0092a6127d336eee1ca7e07222f2bbd94
janikkessler <kessler@w11k.de>
2024-12-29 17:40:08 +0100

    add git version.js

---
+++ src/version/version.ts
@@ -0,0 +1,28 @@
+// IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
+/* tslint:disable */
+/* eslint-disable */
+export const VERSION = {
+    "dirty": true,
+    "raw": "v0.2.17-0-g5e499e7-dirty",
+    "hash": "g5e499e7",
+    "distance": 0,
+    "tag": "v0.2.17",
+    "semver": {
+        "options": {
+            "loose": false,
+            "includePrerelease": false
+        },
+        "loose": false,
+        "raw": "v0.2.17",
+        "major": 0,
+        "minor": 2,
+        "patch": 17,
+        "prerelease": [],
+        "build": [],
+        "version": "0.2.17"
+    },
+    "suffix": "0-g5e499e7-dirty",
+    "semverString": "0.2.17",
+    "version": "0.2.17"
+};
+/* tslint:enable */
